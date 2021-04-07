f916263a97eb36ab7932d5de79976e70fb342f8c
jaydjxing <1693502585@qq.com>
2021-04-07 21:18:41 +0800

    hellogit

---
+++ hellogit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * author: djx
+ * created on: 2021/4/7 21:08
+ * description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
